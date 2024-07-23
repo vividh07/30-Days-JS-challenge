8f5fefcf560abf31db593c0a56f63b42d2d07489
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:13:47 +0530

    Day 5

---
+++ Day 5/index.js
@@ -0,0 +1,116 @@
+// Task 1
+
+// function num(n){
+//     if(n%2===0)
+//     {
+//         console.log("Number is Even");
+        
+//     }
+//     else {
+//         console.log("Number is Odd");
+        
+//     }
+// }
+
+// num(20)
+
+
+//Task 2
+
+// function square(a){
+//     return a*a
+// }
+
+// console.log(square(45))
+
+
+// Task 3
+
+// function max(a,b){
+//     if(a>b){
+//         console.log("a is the greatest");
+//     }
+//     else if(b>a){
+//         console.log("b is the greatest");
+//     }
+//     else{
+//         console.log("both are equal");
+//     }
+// }
+
+// max(3,34)
+
+//Task 4
+
+// function stng(a,b){
+// console.log(a+b);
+// }
+
+// stng("hello","world")
+
+//Task 5
+
+// const sum = (a,b)=>{
+// console.log(a+b);
+// }
+
+// sum(2,5)
+
+//Task 6
+
+// const find = (a)=>{
+//    if( a.includes("h")){
+//     console.log("True");
+//    }
+//    else{
+//     console.log("false");
+//    }
+// }
+// find(`hello`)
+
+//Task 7
+
+// function pro(a,b=8){
+//     console.log(a*b);
+// }
+
+// pro(6)
+
+// Task 8
+
+// function greet(a,b=60){
+//     console.log(`Welcome ${a} your age is ${b}`);
+// }
+
+// greet("Vividh")
+
+// Task 9
+
+// function print(){
+//     console.log("hello world");
+// }
+
+// function times(a,fun){
+//     for(i=0;i<=4;i++){
+//         fun()
+//     }
+// }
+
+// times(4,print)
+
+// Task 10
+
+
+// function sub(n){
+// console.log(n);
+// }
+// function sum(a,sub){
+//      a = a+20
+//      sub(a)
+// }
+
+// function higherFun(a,sum,sub){
+// sum(a,sub)
+// }
+
+// higherFun(5,sum,sub)
