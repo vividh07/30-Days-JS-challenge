a9b8005ee7da5d89a50caf6d83e746343a2f585c
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:14:18 +0530

    Day 7

---
+++ Day 7/index.js
@@ -0,0 +1,93 @@
+// // Task 1
+
+// const books = {
+//     title:"Superman",
+//     author:"Tony stark",
+//     year:2012,
+//     stng : function(){
+//         return `${this.author} ${this.title}`
+//     },
+
+//     change: function(){
+//        books['year'] = 2045
+//     }
+// }
+
+// console.log(books);
+
+// // Task 2
+
+// console.log(books.author);
+// console.log(books.title);
+
+// // Task 3
+
+// console.log(books.stng());
+
+// // Task 4
+
+// books.change()
+// console.log(books);
+
+
+// // Task 5
+
+// const library = {
+//     books:[{
+//         name:"atomic bomb",
+//         author:"doremon"
+//     },{
+//         name:"nobita",
+//         author:"ironman"
+//     },{
+//         name:"doremon",
+//         author:"ninja hatodi"
+//     }],
+//     name:"abcd",
+//     year:2012
+// }
+
+// console.log(library.books);
+
+// // Task 7
+
+// const books = {
+//         title:"Superman",
+//      author:"Tony stark",
+//         year:2012,
+//         stng : function(){
+//              return `${this.author} ${this.title}`
+//         }}
+
+//         console.log(books.stng());
+
+
+
+// // Task 8 
+
+// const books = {
+//     name:"ironman",
+//     author:"stan",
+//     year:2010,
+//     rating:10
+// }
+
+// for(i in books){
+//     console.log(i);
+//     console.log(books[i]);
+// }
+
+
+
+// // Task 9
+
+
+// const books = {
+//     name:"ironman",
+//     author:"stan",
+//     year:2010,
+//     rating:10
+// }
+
+// console.log(Object.keys(books));
+// console.log(Object.values(books));
