d34d6a05a4a9c91b25aed97530539df62fc2fcb0
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:13:11 +0530

    Day 2

---
+++ Day 2/ex.js
@@ -0,0 +1,36 @@
+// var a = 3
+// console.log(a);
+
+// let b = "30 days js challenge"
+// console.log(b);
+
+// const c = true
+// console.log(c);
+
+// var d = 2
+// console.log(typeof d);
+
+// var e = "string"
+// console.log(typeof e);
+
+// var f = false
+// console.log(typeof f);
+
+// var g = {
+//     key:"value"
+// }
+// console.log(typeof g);
+
+// var h = ["hello",1,3,"wr"]
+// console.log(typeof h);
+
+
+// let a = 2
+// a = 5
+// console.log(a);
+
+
+// const a = 5
+// a = 10             this will give error because const value cannot be reassign 
+// console.log(a);
+
---
+++ Day 2/index.js
@@ -0,0 +1,127 @@
+//operators
+// Sum
+
+// function sum(a,b){
+// console.log(a+b);
+// }
+
+// sum(2,4);
+
+//Subtraction
+
+// function sub(a,b){
+//  console.log(a-b);
+// }
+
+// sub(9,4)
+
+// Multiply
+
+// function mul(a,b){
+//     console.log(a*b);
+// }
+
+// mul(9,4)
+
+
+//Divide
+
+// function div(a,b){
+//     console.log(a/b);
+// }
+
+// div(9,3)
+
+// remainder and result
+
+// function rem(a,b){
+//     console.log(a%b);
+// }
+
+// rem(10,3)
+
+//+=
+
+// function op(a){
+//     console.log(a+=5);
+// }
+
+// op(90)
+
+//-=
+
+// function op(a){
+//     console.log(a-=30);
+// }
+
+// op(56)
+
+//comparison operator
+
+// function com(a,b){
+// console.log(a>b);
+// }
+
+// com(9,4)
+
+// function com(a,b){
+//     console.log(a<b);
+//     }
+    
+// com(9,4)
+
+
+// function com(a,b){
+//     console.log(a==b);
+//     }
+    
+//com(9,4)
+
+
+// function com(a,b){
+//     console.log(a===b);
+//     }
+    
+// com(9,4)
+
+// function logicalOp(a,b){
+//     if(a==b && a===b){
+//         console.log("true");
+//     }
+//     else{
+//         console.log("false");
+//     }
+// }
+
+// logicalOp(9,9)
+
+// function logicalOp(a,b){
+//     if(a>=b || a===b){
+//         console.log("true");
+//     }
+//     else{
+//         console.log("false");
+//     }
+// }
+
+// logicalOp(9,7)
+
+// function logicalOp(a){
+//     if(!a){
+//         console.log("true");
+//     }
+//     else{
+//         console.log("false");
+//     }
+// }
+
+// logicalOp(1)
+
+
+//Ternary operator
+
+// function ternary(a,b){
+// console.log((a>b) ? "true" : "flase");
+// }
+
+// ternary(9,3)
