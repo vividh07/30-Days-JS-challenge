2b69135f28c7d05c2299efaa5797e93ac746afde
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:13:58 +0530

    Day  6

---
+++ Day 6/index.js
@@ -0,0 +1,95 @@
+// //Day 6
+
+// // Task 1
+
+// const arr = [1,2,3,4,5]
+// console.log(arr);
+
+// // Task 2
+
+// console.log(arr[0]);
+// console.log(arr[arr.length-1])
+
+// // Task 3
+
+// arr.push(6)
+// console.log(arr);
+
+// // Task 4
+
+// arr.pop()
+// console.log(arr);
+
+// // Task 5
+
+// arr.shift()
+// console.log(arr);
+
+// // Task 6
+
+// arr.unshift(87)
+// console.log(arr);
+
+// // Task 7
+
+// const newArray = [10,23,45,56,76,23]
+
+// const array1 = newArray.map((e)=>{
+//      return  e*2
+// })
+// console.log(array1);
+
+// // Task 8
+
+// const array = [12,45,35,3654,23,897,0,-45,734]
+
+// const newArray = array.filter((e)=>e>100)
+
+// console.log(newArray);
+
+// // Task 9
+
+// const array = [12,45,35,3654,23,897,0,-45,734]
+
+// const newArray = array.reduce((acc,curval)=>{
+//   return acc + curval
+// },0)
+
+// console.log(newArray);
+
+// // Task 10 
+
+// const array = [12,45,35,3654,23,897,0,-45,734]
+
+// for(i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+
+// // Task 11
+
+// const array = [12,45,35,3654,23,897,0,-45,734]
+
+// array.forEach(element => {
+//     console.log(element);
+// });
+
+// // Task 12
+
+// const arr = []
+// row = 3
+// column = 3
+
+// for(i=0;i<row;i++){
+//     arr[i] = []
+//     for(j=0;j<column;j++){
+//         arr[i][j] = j
+//     }
+// }
+
+// console.log(arr);
+
+// // Task 13
+
+// const arr = [[1,2,3,],[4,5,6],[7,8,9]]
+
+// console.log(arr[1][1]);
