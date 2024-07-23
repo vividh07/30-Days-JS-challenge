043e8d24a44a914cbb1ea4ef8bee11b74a40bd46
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:14:32 +0530

    Day 8

---
+++ Day 8/index.js
@@ -0,0 +1,87 @@
+// // task 1
+
+// const name = "mayank"
+// const age = 780
+// console.log(`${name} age is ${age}`);
+
+// // Task 2
+ 
+// console.log(`hello 
+//     how are you
+//     good 
+//     fine`);
+
+// // Task 3
+
+// const arr = [1,2,34,656,75676,75,567567683,3,232]
+
+// const [a,b] =  arr
+// console.log(a);
+// console.log(b);
+
+// // Task 4
+
+// const books = {
+//     name:"ironman",
+//     author:"stanli",
+//     year:2010
+// }
+
+// const{name,author,year} = books
+// console.log(name);
+// console.log(author);
+// console.log(year);
+
+// // Task 5
+
+// const arr = [1,2,3,45,6]
+// const newArr = [...arr,12,3445,4567,]
+// console.log(newArr);
+
+// // Task 6 
+
+// function sum(...val){
+//     let add = 0
+//     for(const i of val){
+//                 add +=  i
+//             }
+//             return add;
+// }
+
+// console.log(sum(2,4,5,6,7,9))
+
+// // Task 7
+
+// function product(a,b=83){
+//     return a*b
+// }
+
+// console.log(product(45,78));
+// console.log(product(45));
+
+// // Task 8  
+
+// const data = {
+//     first_name:"mayank",
+//     last_name:"choudhary",
+//     age:500,
+//     info: function(){
+//         return `Age of ${this.first_name} ${this.last_name} is ${this.age} `
+//     }
+
+// }
+
+// console.log(data.info());
+
+// // Task 9
+
+// const price = {
+//     name:"tea",
+//     price:30,
+//     qty:45,
+//     total:function(){
+//         return this.price*this.qty
+//     }
+// }
+
+//  console.log(price.total());
