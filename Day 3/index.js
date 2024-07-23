2028323c8db72bea10dc3810f1ce2c71c5beb1c5
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:13:27 +0530

    Day 3

---
+++ Day 3/index.js
@@ -0,0 +1,128 @@
+// if-else statement
+ // Task 1
+
+// const a = 5
+// const a = -5
+// const a = 0
+
+// if(a>0){
+//     console.log("Positive number");
+
+// }
+// else if(a<0){
+//     console.log("Negative number");
+// }
+// else{
+//     console.log("Number is zero");
+// }
+
+//Task 2
+
+// const age = 14
+
+// if(age >= 18){
+//     console.log("Eligible to vote");
+// }
+// else if(age < 18){
+//     console.log("Not eligible to vote");
+// }
+
+
+//If Else
+
+// Task 3
+
+// const a = 5
+// const b = 10
+// const c = 2
+
+// if(a>b && a>c){
+//     console.log("a is greatest");
+// }
+// else if(b>c && b>a){
+//     console.log("b is greatest");
+// }
+// else{
+//     console.log("c is greatest");
+// }
+
+
+//Switch case
+
+// Task 4
+
+// const day = 5
+
+// switch (day) {
+//     case 1: console.log("Monday")
+//     break;
+
+//     case 2: console.log("tuesday");
+//     break;
+
+//     case 3: console.log("Wednesday")
+//     break;
+
+//     case 4: console.log("Thursday")
+//     break;
+
+//     case 5:  console.log("Friday")
+//     break;
+
+//     case 6:  console.log("Saturday")
+//     break;
+
+//     case 7:  console.log("Sunday")
+//     break;   
+// }
+
+
+// Task 5
+
+// const score = 85
+
+// switch (true) {
+//     case score>90: console.log("A")
+//     break;
+
+//     case score>=80: console.log("B");
+//     break;
+
+//     case score>70: console.log("C")
+//     break;
+
+//     case score>60: console.log("D")
+//     break;
+
+//     case score>50:  console.log("E")
+//     break;
+
+//     case score>40:  console.log("F")
+//     break;
+
+     
+// }
+
+
+
+// Task 6 
+// const num = 33
+
+// console.log(num/2==0 ? "even" : "odd");
+
+
+// Task 7
+
+// const years = [2020, 2021, 2000, 1900];
+
+// for (let i = 0; i < years.length; i++) {
+//     let year = years[i];
+//     let isLeapYear = false;
+
+    
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//         isLeapYear = true;
+//     }
+
+//     console.log(`${year} is a leap year? ${isLeapYear}`);
+// }
