247b7070674f74a518cab36e05779ef3f3202e1f
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-07-23 18:13:38 +0530

    Day  4

---
+++ Day 4/index.js
@@ -0,0 +1,92 @@
+// Task 1
+
+// for(i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+
+// Task 2
+
+// for(i=1;i<=10;i++){
+//     console.log(`5 * ${i} = ${5*i} `);
+// }
+
+
+// Task 3
+
+// let i = 1
+
+// total =0
+// while(i <= 10){
+//     total += i  
+//     i++
+// }
+// console.log(total);
+
+
+// Task 4 
+
+// let i = 10
+
+// while(i>=1){
+// console.log(i);
+//     i--
+// }
+
+
+// Task 5
+
+// let i = 1
+// do{
+//     console.log(i);
+//     i++
+// }
+// while(i<=5){
+// }
+
+
+// Task 6
+
+// let i = 1
+// let n= 5
+// let fact = 1
+// do{
+//     fact *= i
+//     i++
+    
+// }while(i<= n){
+    
+//     console.log(fact);
+// }
+
+
+// Task 7
+
+// let rows = 5; 
+
+// for (let i = 1; i <= rows; i++) {
+//     let str = '';
+//     for (let j = 1; j <= i; j++) {
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+
+
+// Task 8
+
+// for(i=1;i<=10;i++){
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// Task 9
+
+// for(i=1;i<=10;i++){
+//     if(i==7){
+//         break;
+//     }
+//     console.log(i);
+// }
